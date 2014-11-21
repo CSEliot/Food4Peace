6a7ab996810ebb9ae95b73d90b971ffd06bcb8e8
eliot2 <eliot2@ECS005APC01.ad.umbc.edu>
2014-11-20 19:41:12 -0500

    Animations done, working on menus

---
+++ Assets/DataTracker.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DataTracker : MonoBehaviour {
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    private int p1_item;
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void assignItemNum(int player, int item)
+    {
+        //0: water, 1: grain, 2:soup
+        p1_item = item;
+    }
+}
---
+++ Assets/LevelSelect.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSelect : MonoBehaviour {
+
+    public GUIStyle levelSelectStyle;
+
+    public GUIStyle leftButton;
+    public GUIStyle rightButton;
+
+    public float spaceWidth; 
+
+    private bool leftPressed;
+    private bool rightPressed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {   
+        Rect boxRect = new Rect(Screen.width/2f, Screen.height*.2f, 100f, 20f);
+        GUI.Box(boxRect, "Select a level!", levelSelectStyle);
+
+        Rect areaRect = new Rect(Screen.width*.2f, Screen.height*.3f, Screen.width, Screen.height*.3f);
+        GUILayout.BeginArea(areaRect);
+            GUILayout.BeginHorizontal();
+                leftPressed = GUILayout.Button("Left", leftButton);
+                GUILayout.Space(spaceWidth);
+                rightPressed = GUILayout.Button("Right", rightButton);
+            GUILayout.EndHorizontal();
+        GUILayout.EndArea();
+        //  GUILayout.BeginArea(
+    }
+
+
+}
